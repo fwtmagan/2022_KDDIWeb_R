b757d43a2a7e1af8e4a3d1db26c4d2771fdb1870
M <fwtmagan@gmail.com>
2023-01-08 17:17:58 +0900

    0108

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict'
+
+$(function () {
+    $(".btn").on("click", function () {
+        $(this).closest("div").css("display", "none");
+        id = $(this).attr("href");
+        $(id).addClass("fit").show("slow");
+    });
+});
